2febe9a25b8bd170aaa5f24b56cdc4cfb6d02bce
Narmin Murshudova <119409921+NarminM11@users.noreply.github.com>
2023-12-18 00:11:32 +0400

    some changes

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Home /> */}
-    <FlashCards />
+    <Home />
+    {/* <FlashCards /> */}
     {/* <App /> */}
   </React.StrictMode>
 );
