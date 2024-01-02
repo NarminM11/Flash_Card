eba077ebb606745126b6e7b4a6f076f33fca2943
Narmin Murshudova <119409921+NarminM11@users.noreply.github.com>
2024-01-02 18:41:18 +0400

    navbar changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,49 +1,52 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Menu } from "antd";
+import {
+  HomeOutlined,
+  BookOutlined,
+  MailOutlined,
+  GithubOutlined,
+  InstagramOutlined,
+} from "@ant-design/icons";
 import "../assets/Navbar.css";
+
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="logo">
-        {" "}
-        <img
-          src="https://cdn.dribbble.com/users/5214489/screenshots/16572960/media/6b27c24edb023951054ce638ca40592f.jpg?resize=400x0"
-          alt="Logo"
-          width="60"
-          height="60"
-        />
+      <div>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1" icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="2" icon={<BookOutlined />}>
+            <Link to="/flashcards">FlashCards</Link>
+          </Menu.Item>
+          <Menu.Item key="3" icon={<MailOutlined />}>
+            <Link to="/contact">Contact</Link>
+          </Menu.Item>
+          <Menu.Item key= "4" >
+          <span className="social-icons">
+          <a
+            className="social-media"
+            href="https://github.com/NarminM11"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GithubOutlined />
+          </a>
+          <a
+            className="social-media"
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <InstagramOutlined />
+          </a>
+        </span>
+          </Menu.Item>
+        </Menu>
+        
       </div>
-      <ul className="nav-list">
-        <li className="nav-item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/flashcards">FlashCards</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-      <span className="social-icons">
-        <a
-          className="social-media"
-          href="https://github.com/NarminM11"
-          target="_blank"
-          rel=""
-        >
-          GitHub
-        </a>
-        <a
-          className="social-media"
-          href="https://www.instagram.com"
-          target="_blank"
-          rel=""
-        >
-          Instagram
-        </a>
-      </span>
-    </nav>
   );
 };
 
